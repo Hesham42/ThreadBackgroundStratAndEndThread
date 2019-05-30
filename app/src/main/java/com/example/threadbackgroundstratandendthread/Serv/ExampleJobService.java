5e6030120de91958b7689ede2f647499053991e7
hesham <hmostafa3bd@gmail.com>
2019-05-30 08:53:54 +0200

    ButtonPorblem

--- app/src/main/java/com/example/threadbackgroundstratandendthread/Serv/ExampleJobService.java
+++ app/src/main/java/com/example/threadbackgroundstratandendthread/Serv/ExampleJobService.java
@@ -2,10 +2,14 @@ package com.example.threadbackgroundstratandendthread.Serv;
 
 import android.app.Notification;
 import android.app.PendingIntent;
+import android.app.Service;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
@@ -16,11 +20,12 @@ import com.example.threadbackgroundstratandendthread.R;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import static com.example.threadbackgroundstratandendthread.Serv.App.CHANEL_1_ID;
 
-public class ExampleJobService extends JobService {
-    private boolean jobCancelled = false;
+public class ExampleJobService extends Service {
     //app notifcation
     Intent activeIntent;
     PendingIntent pendingIntent;
@@ -36,29 +41,78 @@ public class ExampleJobService extends JobService {
     XMLParser xmlParser;
     ArrayList<HashMap<String, String>> menuItems = new ArrayList<HashMap<String, String>>();
 
+    public int counter=0;
+    private Timer timer;
+    private TimerTask timerTask;
+    long oldTime=0;
+
+    public ExampleJobService(Context applicationContext) {
+        super();
+        Log.i("HERE", "here I am!");
+
+
+    }
+
+    public ExampleJobService() {
+    }
+
     @Override
-    public boolean onStartJob(JobParameters params) {
-        Log.d(TAG, "Job started");
-            doBackgroundWork(params);
-
-        return true;
-    }
-
-    private void doBackgroundWork(JobParameters params) {
-        xmlParser = new XMLParser();
-        menuItems = xmlParser.ParsingData();
-        NotificationMenuItem(menuItems,getApplicationContext());
-        jobFinished(params, false);
-
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        startTimer();
+        return START_STICKY;
     }
 
     @Override
-    public boolean onStopJob(JobParameters params) {
-        Log.d(TAG, "Job cancelled before completion");
-        jobCancelled = true;
-        return true;
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i("EXIT", "ondestroy!");
+        Intent broadcastIntent = new Intent(this, MyBroadcastReceiver.class);
+        broadcastIntent.putExtra("Start","Start");
+        sendBroadcast(broadcastIntent);
+        stoptimertask();
     }
 
+    public void startTimer() {
+        //set a new Timer
+        timer = new Timer();
+
+        //initialize the TimerTask's job
+        initializeTimerTask();
+
+        //schedule the timer, to wake up every 1 second
+        timer.schedule(timerTask,1000 , 60000); //
+    }
+
+
+    /**
+     * it sets the timer to print the counter every x seconds
+     */
+    public void initializeTimerTask() {
+        timerTask = new TimerTask() {
+            public void run() {
+                xmlParser = new XMLParser();
+                menuItems = xmlParser.ParsingData();
+                NotificationMenuItem(menuItems, getApplicationContext());
+            }
+        };
+    }
+    /**
+     * not needed
+     */
+    public void stoptimertask() {
+        //stop the timer, if it's not already null
+        if (timer != null) {
+            timer.cancel();
+            timer = null;
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 
     private void NotificationMenuItem(ArrayList<HashMap<String, String>> menuItems , Context context) {
         Log.wtf(TAG,menuItems.toString());
@@ -87,7 +141,6 @@ public class ExampleJobService extends JobService {
                     .setAutoCancel(true)
                     .build();
 
-            notification.flags = Notification.FLAG_AUTO_CANCEL;
             managerCompat.notify(Integer.parseInt(menuItems.get(i).get(KEY_ID)), notification);
 
         }
